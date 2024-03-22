5f6ec1febd1551e35ffa7b4738ee8a4ed162c86a
Raveena Ingale <ingale.r@northeastern.edu>
2024-03-21 22:56:07 -0400

    deleted healthdao

--- src/main/java/edu/neu/neuconnect/controller/rest/HealthController.java
+++ src/main/java/edu/neu/neuconnect/controller/rest/HealthController.java
@@ -1,14 +1,11 @@
 package edu.neu.neuconnect.controller.rest;
 
-import edu.neu.neuconnect.dao.HealthDAO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 
 @Controller
