4d49ad034a3b592e4110ad35b9430ef8c5abf513
Raveena Ingale <ingale.r@northeastern.edu>
2024-03-21 22:52:38 -0400

    fix: removed unrealted file

--- LeetCode/StringLength.java
+++
@@ -1,9 +0,0 @@
-//Demonstrate length
-public class StringLength{
-    public static void main(String args[]){
-        String s1 = "Hello World";
-        int num = s1.length();
-
-        System.out.println(num);
-    }
-}
