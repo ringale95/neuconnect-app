cefecaf482dbefa90baef574ed834ff0e6d44f91
Raveena Ingale <ingale.r@northeastern.edu>
2024-03-22 21:09:11 -0400

    fi:retry

--- src/main/java/edu/neu/neuconnect/NeuconnectApplication.java
+++ src/main/java/edu/neu/neuconnect/NeuconnectApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NeuconnectApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Env: " + System.getenv("SPRING_DATASOURCE_URL"));
 		SpringApplication.run(NeuconnectApplication.class, args);
 	}
 
